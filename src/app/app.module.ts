3f8eabaa021cd9804fde0940d4b781416fa8ce71
Bharatdaka <bharat.daka26@gmail.com>
2018-12-04 14:00:13 +0530

    reorder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TdlistComponent } from './components/tdlist/tdlist.component';
 import { AddtaskComponent } from './components/addtask/addtask.component';
+import { FormsModule } from '@angular/forms';
 import { LabelcasePipe } from './pipes/labelcase.pipe';
 
 @NgModule({
